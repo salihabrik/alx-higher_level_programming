12e4ee130e7df7191c645cce652b38aa3d5cac95
salihabrik <briksaliha@gmail.com>
2023-06-12 14:44:03 -0700

    no one care

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,10 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     first-element = tuple_a + (0, 0)
     second-element = tuple_b + (0, 0)
 
-    results1 = 0
-    results2 = 0
 
-    results1 += first-element[0] + second-element[0]
-    results2 += first-element[1] + second-element[1]
+    results = (first-element[0] + second-element[0],first-element[1] + second-element[1])
+    
 
-    return (results1, results2)
+    return (results)

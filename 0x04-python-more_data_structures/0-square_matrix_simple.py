cb6620b1d559d1bbf01d27ce49fc9502c71e3f53
salihabrik <briksaliha@gmail.com>
2023-06-14 16:27:08 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=[]):
     result =  [[0 for _ in range(len(row))] for row in matrix]
-     for i in range(len(matrix)):
+    for i in range(len(matrix)):
         for s in range(len(matrix[i])):
             result[i][s] = matrix[i][s] ** 2
 

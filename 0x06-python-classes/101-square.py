5169fa2c87778648bddc606d7848ead36a6992d8
root <root@DESKTOP-0ASGO79.localdomain>
2023-06-28 01:53:48 +0200

    object oriented programming

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -44,16 +44,4 @@ class Square:
 
     def area(self):
         """Return the current area of the square."""
-        return (self.__size * self.__size)
-
-    def my_print(self):
-        """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        return (self.__size * self.__s

eb53bf930510e81283dd2e3eb1f172711016c9bc
root <root@DESKTOP-0ASGO79.localdomain>
2023-06-28 01:44:08 +0200

    object oriented programming

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
 
     def area(self):
         """Return the current area of the square."""
-        return (self__size * self.__size)
+        return (self.__size * self.__size)
 
     def my_print(self):
         """Print the square with the # character."""

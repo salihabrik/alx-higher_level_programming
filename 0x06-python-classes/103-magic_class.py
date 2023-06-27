b1fa816ffbba0610b5b06da28ed781cf2d6a86f4
root <root@DESKTOP-0ASGO79.localdomain>
2023-06-28 01:55:35 +0200

    object oriented programming

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class MagicClass"""
+import math
+
+
+class MagicClass:
+    """This represents a circle"""
+    def __init__(self, radius=0):
+        """Initializes the Magic Class"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius

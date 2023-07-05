26809425cc248f6484b218ce6accab1c4122a308
salihabrik <briksaliha@gmail.com>
2023-07-05 18:30:06 +0200

    A number of studies have shown testing not very effective at finding bugs

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,29 @@
+#!/usr/bin/python3
+"""defines function to scalar divde matrix"""
+
+
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by a given number."""
-    if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-    if not all(isinstance(row, list) for row in matrix) or not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    """divides matrix by scalar integer, rounded to two decimal places"""
+    import decimal
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(error_msg)
+    len_rows = []
+    row_count = 0
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(error_msg)
+        len_rows.append(len(row))
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError(error_msg)
+        row_count += 1
+    if len(set(len_rows)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
-    
-    if not isinstance(div, (int, float)):
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    
-    if div == 0:
+    if int(div) == 0:
         raise ZeroDivisionError("division by zero")
-    
-    return [[round(num / div, 2) for num in row] for row in matrix]
+    new_matrix = list(map(lambda row:
+                          list(map(lambda x: round(x/div, 2), row)), matrix))
+    return new_matrix

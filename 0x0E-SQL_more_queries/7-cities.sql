f013070397f14e76edb6a00bcd0b5935ab0c03fb
salihabrik <briksaliha@gmail.com>
2023-08-18 00:02:18 +0200

    (:

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
--- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
--- creates a database
+-- create the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- use a database
 USE hbtn_0d_usa;

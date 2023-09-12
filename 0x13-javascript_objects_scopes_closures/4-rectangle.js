3c25166d4ddae23d13a1e74d33a8ee4184d7bc69
salihabrik <briksaliha@gmail.com>
2023-09-13 01:06:55 +0200

    class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let s = 0; s < this.height; s++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;

58fec973edb48b12bbdea2e523ca7fb08b0e2ca3
salihabrik <briksaliha@gmail.com>
2023-09-13 01:48:35 +0200

    imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+
+const sortedDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (!sortedDict[occurrences]) {
+    sortedDict[occurrences] = [];
+  }
+  sortedDict[occurrences].push(userId);
+}
+
+console.log(sortedDict);

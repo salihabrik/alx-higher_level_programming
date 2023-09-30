0f7c4433c40f55d73622c8d2c118dce96c44819e
salihabrik <briksaliha@gmail.com>
2023-09-30 15:02:55 +0200

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,3 +4,23 @@
 import requests
 import sys
 
+if __name__ == "__main__":
+
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    # GitHub API URL
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        owner_name, repository_name)
+
+    params = {'per_page': 10}
+
+    # send a GET request to the GitHub API
+    response = requests.get(url, params=params)
+
+    # parse the JSON response
+    commits = response.json()
+    for commit in commits:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")

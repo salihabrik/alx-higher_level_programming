9493b24cd5ed83536181bdf2513c42576a45b5ce
salihabrik <briksaliha@gmail.com>
2023-09-30 15:14:46 +0200

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""using the GitHub API to list commits GITHUB_API_URL = "https://api.github.com"""
+"""using the GitHub API to list commits URL = "https://api.github.com"""
 
 import requests
 import sys

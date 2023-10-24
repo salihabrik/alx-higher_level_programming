1d1cfafd95effb65d3ed8520f8e104809ddc03b4
salihabrik <briksaliha@gmail.com>
2023-10-24 23:22:24 +0200

    README

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (error, data) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(data);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });

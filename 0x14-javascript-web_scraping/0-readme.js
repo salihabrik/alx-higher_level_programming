74479dc4b4f300df854a21af652fa8bcc2562e31
salihabrik <briksaliha@gmail.com>
2023-10-24 23:17:51 +0200

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,14 @@
-# JavaScript Web Scraping Project
+#!/usr/bin/node
+// import module fs to read a file
 
-This project consists of several JavaScript scripts for web scraping and working with APIs. Each script performs a specific task related to web scraping and API requests.
+const fs = require('fs');
 
-## Prerequisites
+const filePath = process.argv[2];
 
-- Node.js (v14.x)
-- npm (Node Package Manager)
-- Required npm modules:
-  - `request`
-
-You can install the required npm modules using the following commands:
-
-```bash
-$ sudo npm install request --global
+fs.readFile(filePath, 'utf-8', (error, data) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(data);
+    }
+});

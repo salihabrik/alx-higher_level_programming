e149b38468001b65ae634c0c89476016e26f9112
salihabrik <briksaliha@gmail.com>
2023-10-26 23:16:38 +0200

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,14 @@ const apiUrl = process.argv[2];
 const characterId = 18;
 
 request(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const filmsData = JSON.parse(body).results;
-    const moviesWithWedgeAntilles = filmsData.filter((film) =>
-      film.characters.includes
-      (`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
+    if (!error && response.statusCode === 200) {
+        const filmsData = JSON.parse(body).results;
+        const moviesWithWedgeAntilles = filmsData.filter((film) =>
+            film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+        );
 
-    console.log(moviesWithWedgeAntilles.length);
-  } else {
-    console.error(error);
-  }
+        console.log(moviesWithWedgeAntilles.length);
+    } else {
+        console.error(error);
+    }
 });
